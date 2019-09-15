3af906c9525988dabe1d58ce19def0554d341fb4
Mihailo Boskovic <mihailob5017@gmail.com>
2019-09-15 23:02:26 +0200

    got stuck here

--- public/chat.js
+++ public/chat.js
@@ -12,24 +12,23 @@ var user = "";
 var socket = io();
 
 //onload a popup will ask the user to enter his username,and once a user submits his name,it will popout of the screen and enable for the user to chat with other users
-window.addEventListener("load", () => {
-  document.getElementById("login").style.animationName = "popup";
-  document.getElementById("submit-button").addEventListener("click", e => {
-    e.preventDefault();
-    if (document.getElementById("login-name").value !== "") {
-      user = document.getElementById("login-name").value;
-      document.getElementById("login").style.animationName = "popout";
-      setTimeout(() => {
-        document.body.removeChild(document.getElementById("login"));
-        EnableChat(user, socket);
-      }, 1200);
-    } else {
-      alert("you need to fill in the name");
-    }
-  });
+
+document.getElementById("login").style.animationName = "popup";
+document.getElementById("submit-button").addEventListener("click", e => {
+  e.preventDefault();
+  if (document.getElementById("login-name").value !== "") {
+    user = document.getElementById("login-name").value;
+    document.getElementById("login").style.animationName = "popout";
+    setTimeout(() => {
+      document.body.removeChild(document.getElementById("login"));
+      EnableChat();
+    }, 1200);
+  } else {
+    alert("you need to fill in the name");
+  }
 });
 //EveryThins is writen in this main function
-const EnableChat = (user, socket) => {
+const EnableChat = () => {
   //welcome board for the current user
   document.body.innerHTML += `<div id='welcome-msg'>Welcome ${user} to Chat's</div>`;
   document.getElementById("welcome-msg").style.animationName = "fade";
@@ -37,6 +36,16 @@ const EnableChat = (user, socket) => {
   setTimeout(() => {
     document.body.removeChild(document.getElementById("welcome-msg"));
     document.body.innerHTML += `<div id="user-name">${user}</div>`;
+    document.body.innerHTML += `
+    <div id='chat-area'>
+    <ul id='chat-room'>
+    </ul>
+      <div id='msg-container'>
+        <input type='text' autocomplete='off' id='msg' placehoalder='Enter Message'/>
+        <button id='msg-button'>Send</button>
+      </div>
+    </div>`;
+    CreateChat();
   }, 3450);
   //
   //
@@ -55,3 +64,37 @@ const EnableChat = (user, socket) => {
 };
 
 //ask the user if
+const CreateChat = () => {
+  const msgBtn = document.getElementById("msg-button");
+  msgBtn.addEventListener(
+    "click",
+    () => {
+      if (document.getElementById("msg").value !== "") {
+        socket.emit("chat message", {
+          user: user,
+          message: document.getElementById("msg").value
+        });
+        document.getElementById("chat-room").innerHTML += `<li class='msg-sent'>
+        <h5>You:</h5>
+        <p>${document.getElementById("msg").value}</p>
+    </li>`;
+        document.getElementById("msg").value = "";
+      } else {
+        alert("You need to enter something");
+      }
+    },
+    false
+  );
+  //
+  //
+  //
+  //
+
+  socket.on("chat message", obj => {
+    const { user, message } = obj;
+    document.getElementById("chat-room").innerHTML += `<li class='msg-recived'>
+        <h5>${user}:</h5>
+        <p>${message}</p>
+    </li>`;
+  });
+};
