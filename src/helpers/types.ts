ab437be3ed13d0d1e4b90b45681de3337ce7a020
mihailo-boskovic-ey <99258303+mihailo-boskovic-ey@users.noreply.github.com>
2023-01-31 16:58:59 +0100

    Update types.ts

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -1,24 +1,66 @@
-export interface LoginProps {}
-
-export interface VstackType {
-  as: any | undefined;
-  w: object;
-  m: string;
-  justify: string;
-  h: string;
-  spacing: string;
-}
-
-export const VStackProps: VstackType = {
-  as: 'form',
-  w: { base: '90%', md: '500px' },
-  m: 'auto',
-  justify: 'center',
-  h: '100vh',
-  spacing: '1rem',
+import { SignUpParams, ResponseParams } from './types';
+export const upperCaseName = (name: string): string => {
+  if (name === '') return name;
+  return name.charAt(0).toUpperCase() + name.slice(1);
 };
+export const SIGNUP_URL: string = 'http://localhost:4001/auth/signup';
+export const LOGIN_URL: string = 'http://localhost:4001/auth/login';
 
-export interface FormikConfigType {
-  username: string;
-  password: string;
-}
+//TODO: There must be a better way to do this,find a better way
+export const fetchCredentials = async (
+  url: string,
+  body: SignUpParams
+): Promise<ResponseParams> => {
+  const responseObject = await fetch(url, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  })
+    .then((response) => {
+      if (!response || !response.ok || response.status >= 400)
+        return {
+          data: null,
+          success: false,
+          error: {
+            message: 'Failed to fetch data from the server',
+            displayMessage: 'Something went wrong',
+          },
+        };
+
+      return response.json();
+    })
+    .then((data) => {
+      if (!data && data.length === 0) {
+        return {
+          data: null,
+          success: false,
+          error: {
+            message: 'Failed to fetch data from the server',
+            displayMessage: 'Something went wrong',
+          },
+        };
+      }
+      return {
+        data: {
+          user: data,
+        },
+        success: true,
+        error: null,
+      };
+    })
+    .catch((err) => {
+      return {
+        data: null,
+        success: false,
+        error: {
+          message: err.messages[0],
+          displayMessage: 'Something went wrong',
+        },
+      };
+    });
+
+  return responseObject;
+};
